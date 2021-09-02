89c9fb8060b85f49aab39b23fc2e64a7667b9d39
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-02 12:49:08 -0600

    Creating a Header component

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+      <h1>This is the header</h1>
+      <div className="header__left">
+        <img src="" alt="" />
+
+        <div className="header__search">
+          {/* Search icon */}
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right"></div>
+    </div>
+  )
+}
+
+export default Header
