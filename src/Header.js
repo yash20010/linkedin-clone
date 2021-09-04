e70c33e8aff0318ff2ac2742235a069df647ecd5
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-04 13:11:57 -0600

    Adding icon and search bar

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
 
 function Header() {
   return (
     <div>
-      <h1>This is the header</h1>
       <div className="header__left">
-        <img src="" alt="" />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt="LinkedIn logo"
+        />
 
         <div className="header__search">
           {/* Search icon */}
+          <SearchIcon />
           <input type="text" />
         </div>
       </div>
