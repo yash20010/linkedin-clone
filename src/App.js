c7e7a625ee594f1c5f21480ad19c92b9ad8bc585
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-07 18:36:33 -0600

    Added App Body with Sidebar and Feed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css'
 import Header from './Header'
+import Sidebar from './Sidebar'
+import Feed from './Feed'
 
 function App() {
   return (
@@ -9,9 +11,13 @@ function App() {
       <Header />
 
       {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+      <div className="app__body">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Feed */}
+        <Feed />
+        {/* Widgets */}
+      </div>
     </div>
   )
 }
