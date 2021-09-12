527231271fce7927830e362b9f7eadc4401375ec
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-12 13:41:19 -0600

    Adding firebase firestore and useEffect Hook to add and render posts

--- src/Feed.js
+++ src/Feed.js
@@ -1,19 +1,85 @@
 import CreateIcon from '@material-ui/icons/Create'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Feed.css'
+import InputOption from './InputOption'
+import ImageIcon from '@material-ui/icons/Image'
+import SubscriptionIcon from '@material-ui/icons/Subscriptions'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
+import Post from './Post'
+import { db } from './firebase'
+import firebase from 'firebase'
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    db.collection('posts')
+      .orderBy('timeStamp', 'desc')
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              data: doc.data(),
+            }
+          })
+        )
+      )
+  }, [])
+
+  const sendPost = (e) => {
+    e.preventDefault()
+
+    db.collection('posts').add({
+      name: 'Yash Parekh',
+      description: 'this is a test',
+      message: input,
+      photoUrl: '',
+      timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+    setInput('')
+  }
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
           <form action="">
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit" onClick={sendPost}>
+              Send
+            </button>
           </form>
         </div>
+        <div className="feed__inputOptions">
+          {/* Input Options */}
+          <InputOption title="Photo" Icon={ImageIcon} color="#70B5F9" />
+          <InputOption title="Video" Icon={SubscriptionIcon} color="#E7A33E" />
+          <InputOption title="Event" Icon={EventNoteIcon} color="#C0CBCD" />
+          <InputOption
+            title="Write Article"
+            Icon={CalendarViewDayIcon}
+            color="#7FC15E"
+          />
+        </div>
       </div>
+      {/* Posts */}
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
     </div>
   )
 }
