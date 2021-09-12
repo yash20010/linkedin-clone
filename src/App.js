e6e811e38211d28faf871dd0b6b60c23268495ce
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-12 13:40:03 -0600

    Updating App.js component, adding login, register and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from './features/userSlice'
 import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Feed from './Feed'
+import Login from './Login'
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       {/* Header */}
       <Header />
 
-      {/* App Body */}
-      <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* Feed */}
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
     </div>
   )
 }
