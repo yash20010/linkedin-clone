b6b6c7fe58c62d7738e1f40f1e3f5effe7832436
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-16 17:51:22 -0600

    Adding login and logout to the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
 import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Feed from './Feed'
 import Login from './Login'
+import { auth } from './firebase'
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // usr is logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        )
+      } else {
+        // user is logged out
+        dispatch(logout())
+      }
+    })
+  }, [])
 
   return (
     <div className="app">
