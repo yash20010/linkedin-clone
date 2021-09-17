0f6465ab4e30138d1c93322eff3952bab31d3f72
throne.yash@gmail.com <throne.yash@gmail.com>
2021-09-17 17:54:51 -0600

    Pulling user from Redux store

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './HeaderOption.css'
 import { Avatar } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+import { selectUser } from './features/userSlice'
 
-function HeaderOption({ title, Icon, avatar }) {
+function HeaderOption({ title, Icon, avatar, onClick }) {
+  const user = useSelector(selectUser)
+
   return (
-    <div className="headerOption">
+    <div className="headerOption" onClick={onClick}>
       {Icon && <Icon className="headerOption__icon" />}
-      {avatar && <Avatar className="headerOption__icon" src={avatar} />}
+      {avatar && (
+        <Avatar className="headerOption__icon" src={user?.photoUrl}>
+          {user?.email[0]}
+        </Avatar>
+      )}
       <h3 className="headerOption__title">{title}</h3>
     </div>
   )
